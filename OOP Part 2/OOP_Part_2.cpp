61af90c6878007739748acf38a08389ed0fdf284
S FARID <faridsadirov4@gmail.com>
2025-03-11 00:57:20 +0400

    oop2

--- OOP Part 2/OOP_Part_2.cpp
+++ OOP Part 2/OOP_Part_2.cpp
@@ -1,6 +1,180 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+
+
+class SuperVector
+{
+private:
+    int* _data;
+    size_t _size;
+    size_t _capacity;
+public:
+    SuperVector() : _data(nullptr), _size(0), _capacity(0){}
+
+    
+    SuperVector(size_t size) : _size(size), _capacity(size)
+    {
+        
+        _data = new int[_capacity];
+        
+    }
+
+    
+    
+    SuperVector(const SuperVector& other) : _size(other._size), _capacity(other._capacity)
+    {
+        
+        _data = new int[_capacity];
+        for (size_t i = 0; i < _size; i++)
+        {
+            _data[i] = other._data[i];
+        }
+    }
+
+    int Get(size_t index) const
+    {
+        if (index <= _size && index >= 0)
+        {
+            return _data[index];
+        }
+        return 0;
+    }
+
+    size_t Size() const
+    {
+        return _size;
+    }
+
+    ~SuperVector()
+    {
+        delete[] _data;
+        
+    }
+    
+    void ResizeArray(size_t NewCapacity)
+    {
+        int *TempData = new int[NewCapacity];
+        for (size_t i = 0; i < _size; i++)
+        {
+            TempData[i] = _data[i];
+        }
+        delete[] _data;
+        _data = TempData;
+        _capacity = NewCapacity;
+        
+    }
+
+    void Pop(SuperVector &other)
+    {
+        _size = _size - 1;
+        ResizeArray(_capacity - 1);
+    }
+    
+    void Remove(SuperVector &other, size_t index)
+    {
+        for (size_t i = index; i < _size; i++)
+        {
+            _data[i] = _data[i + 1];
+        }
+
+        ResizeArray(_capacity - 1);
+        _size--;
+    }
+    
+    void FillArray(SuperVector *other)
+    {
+        for (int i = 0; i < _capacity; ++i)
+        {
+            _data[i] = i + 1;
+        }
+
+    }
+
+    void Insert(SuperVector &other, size_t index, int value)
+    {
+        if (_size == _capacity)
+        {
+            ResizeArray(_capacity + 1);
+        }
+        index  -= 1;
+        int* temp_data = new int [_capacity];
+
+        for (size_t i = 0; i < index; i++)
+        {
+            temp_data[i] = _data[i];
+        }
+        
+        temp_data[index] = value;
+
+        for (size_t i = index; i < _size; i++)
+        {
+            temp_data[i + 1] = _data[i];
+        }
+        delete[] _data;
+        _data = temp_data;
+        _size++;
+    }
+
+    void PushBack(SuperVector &other, int value)
+    {
+        if (_size == _capacity)
+        {
+            ResizeArray(_capacity + 1);
+        }
+        
+        _data[_size] = value;
+        _size++;
+        
+    }
+    void Set(SuperVector &other ,size_t index, int value)
+    {
+        _data[index - 1] = value;
+    }
+    
+};
+
+void DisplayVector(const SuperVector& vector)
+{
+    for (int i = 0; i < vector.Size(); ++i)
+    {
+        std::cout<<vector.Get(i)<< " ";
+    }
+}
+
+
 
 int main()
 {
-    std::cout<<"Enter Brand: ";
+
+    SuperVector temp(5);
+    
+    temp.FillArray(&temp);
+
+    DisplayVector(temp);
+    
+    std::cout <<"Fill array" << std::endl;
+    
+    temp.Remove(temp, 2);
+
+    DisplayVector(temp);
+
+    std::cout <<"Remove"<< std::endl;
+
+    temp.Insert(temp, 3, 999);
+
+    DisplayVector(temp);
+
+    std::cout <<"Insert"<<std::endl;
+
+    temp.PushBack(temp, 123);
+
+    DisplayVector(temp);
+
+    std::cout <<"PushBack" <<std::endl;
+
+    temp.Set(temp, 2, 999);
+    
+    DisplayVector(temp);
+    std::cout <<"Set"<<std::endl;
+    return 0;       
 }
